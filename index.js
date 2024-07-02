21d0845ddb7de35ea1da3f94c686b5143d44a483
tomimoure8 <tomimoure8@gmail.com>
2024-07-01 23:22:36 -0300

    actualización

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
+// Pide al usuario ingresar la contraseña del unicornio.
 alert("Ingrese la contraseña secreta del unicornio 🦄");
 alert("Si no la adivina no me hago cargo de las consecuencias 😈");
-
+// Define el objeto con la contraseña y la pista.
 const contrasenas = [
     { nombre: 'unicornio', contrasena: 'ESC', pista: 'Las iniciales de un teclado' },
 ];
-
+// Función para verificar si la contraseña es correcta.
 function verificarContrasena(contrasenaIngresada, contrasenaObjeto) {
     const esCorrecta = contrasenaIngresada === contrasenaObjeto.contrasena;
     console.log("Verificando contraseña: " + (esCorrecta ? 'Correcta' : 'Incorrecta'));
     return esCorrecta;
 }
-
+// Bucle para gestionar los intentos.
 let intentos = 5;
 while (intentos > 0) {
     const intento = prompt("Ingresar contraseña");
@@ -30,7 +31,7 @@ while (intentos > 0) {
         intentos--;
     }
 }
-
+// Mensaje final si se agotan los intentos.
 if (intentos === 0) {
     const pistas = contrasenas.map(obj => "La contraseña del " + obj.nombre + " era: " + obj.contrasena + ", pista: " + obj.pista);
     console.log("Se agotaron todos los intentos. La contraseña era: ", obj.contrasena);
